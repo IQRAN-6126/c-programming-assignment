b970697d0f81670be38833149aa3335efa92b729
IQRAN-6126 <iqrangazi103@gmail.com>
2025-10-26 23:32:19 +0530

    Create vol of a cylinder.c

---
+++ vol of a cylinder.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define PI 3.14
+
+int main()
+{
+    float radius, height, volume;
+    printf("Enter radius and height of the cylinder: ");
+    scanf("%f %f", &radius, &height);
+    volume = PI * radius * radius * height;
+    printf("Volume of cylinder = %.2f\n", volume);
+    return 0;
+}
