79f82aff88a53e43b8ed463f170320040c9ad2c5
IQRAN-6126 <iqrangazi103@gmail.com>
2025-10-26 23:27:14 +0530

    Create square and cube of an integer.c

---
+++ square and cube of an integer.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+
+int main() 
+{ 
+    int number; 
+    printf("\nEnter an integer: "); 
+    scanf("%d", &number); 
+    printf("\nSquare of the number is %d", number * number); 
+    printf("\nCube of the number is %d", number * number * number); 
+    return 0; 
+}
